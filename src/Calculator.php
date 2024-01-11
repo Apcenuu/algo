6f1b98f40e14bf2f1816ca3ac756e2d7309cddb7
Apcenuu <q4nko.apcenuu@gmail.com>
2024-01-11 22:05:32 +0700

    Multiplication

--- src/Calculator.php
+++ src/Calculator.php
@@ -29,7 +29,70 @@ final class Calculator
         return array_reverse($res);
     }
 
-    public function normalize(array $a, int $maxCount): array
+    public function subtraction(array $a1, array $a2): array
+    {
+        $maxCount = max(count($a1), count($a2));
+        $a1 = $this->normalize($a1, $maxCount);
+        $a2 = $this->normalize($a2, $maxCount);
+
+        $flag = 0;
+        $res = [];
+        for ($i = $maxCount - 1; $i >= 0; $i--) {
+            $diff = $a1[$i] - $a2[$i] - $flag;
+            if ($diff < 0) {
+                $flag = 1;
+                $diff = 10 + $diff;
+            } else {
+                $flag = 0;
+            }
+            $res[] = $diff;
+        }
+
+        return array_reverse($res);
+    }
+
+    public function multiplication(array $a1, array $a2): array
+    {
+        $maxCount = max(count($a1), count($a2));
+        $a1 = $this->normalize($a1, $maxCount);
+        $a2 = $this->normalize($a2, $maxCount);
+
+        $remainder = 0;
+        $res = [];
+        for ($i = $maxCount - 1; $i >= 0; $i--) {
+            for ($j = $maxCount - 1; $j >= 0; $j--) {
+                $product = ($a1[$j] * $a2[$i]) + $remainder;
+                if ($product >= 10) {
+                    $remainder = intdiv($product, 10);
+                    $product = $product % 10;
+                } else {
+                    $remainder = 0;
+                }
+                $res[$i][$j] = $product;
+            }
+        }
+
+        $result = [];
+        for ($i = count($res) - 1; $i >= 0; $i--) {
+            for ($j = count($res[$i]) - 1; $j >= 0; $j--) {
+                $result[$i][] = $res[$i][$j];
+            }
+            $result[$i] = array_reverse($result[$i]);
+        }
+
+        $multiplier = [];
+        $itog = $result[count($result) - 1];
+        for ($i = count($result) - 1; $i > 0; $i--) {
+            $multiplier[] = 0;
+            $item1 = $itog;
+            $item2 = array_merge($result[$i - 1], $multiplier);
+            $itog = $this->summ($item1, $item2);
+        }
+
+        return $itog;
+    }
+
+    private function normalize(array $a, int $maxCount): array
     {
         $zerosCount = $maxCount - count($a);
         $result = [];
--- tests/Unit/CalculatorCest.php
+++ tests/Unit/CalculatorCest.php
@@ -4,6 +4,8 @@
 namespace App\Tests\Unit;
 
 use App\Calculator;
+use Codeception\Attribute\DataProvider;
+use Codeception\Example;
 use Tests\Support\UnitTester;
 
 class CalculatorCest
@@ -14,5 +16,42 @@ class CalculatorCest
         $calculator = new Calculator();
         $summ = $calculator->summ([1, 5, 0, 0, 0], [2, 0, 3, 0]);
         $I->assertEquals([1, 7, 0, 3, 0], $summ);
+    }
+
+    public function subtractionTest(UnitTester $I): void
+    {
+        $calculator = new Calculator();
+        $diff = $calculator->subtraction([1, 5, 0, 0, 0], [2, 0, 3, 0]);
+        $I->assertEquals([1, 2, 9, 7, 0], $diff);
+    }
+
+    /**
+     * @uses multiplicationProvider
+     */
+    #[DataProvider('multiplicationProvider')]
+    public function multiplicationTest(UnitTester $I, Example $example): void
+    {
+        $calculator = new Calculator();
+        $product = $calculator->multiplication($example['a1'], $example['a2']);
+        $count = count($example['res']);
+        $I->assertEquals($example['res'], array_slice($product, -$count, $count));
+    }
+
+    protected function multiplicationProvider(): array
+    {
+        return [
+            [
+                'a1' => [2, 5], 'a2' => [3, 0], 'res' => [7, 5, 0],
+            ],
+            [
+                'a1' => [2, 0], 'a2' => [2, 0], 'res' => [4, 0, 0],
+            ],
+            [
+                'a1' => [1, 0, 0], 'a2' => [2, 0], 'res' => [2, 0, 0, 0],
+            ],
+            [
+                'a1' => [1, 2, 3], 'a2' => [4, 5, 6, 7], 'res' => [5, 6, 1, 7, 4, 1],
+            ],
+        ];
     }
 }
